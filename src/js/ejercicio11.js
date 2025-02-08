4715e3739df7a03266ff7564c83e745099b4b232
ARTEMIO GAMALIEL MARTINEZ PACHECO <martinezkatia133@gmail.com>
2025-02-07 19:31:59 -0600

    ejercicio 16

---
+++ src/js/ejercicio11.js
@@ -0,0 +1,12 @@
+/**
+ * Ejercicio 11
+Calcular el salario semanal de un empleado, considerando un costo por hora y las horas trabajadas, deberá 
+mostrar en consola las horas trabajadas y el salario semanal, utiliza Template String.
+ */
+
+let costosHora = 15;
+let horasTrab = 40;
+
+const salarioSemanal = costosHora * horasTrab;
+
+console.log(`Horas trabajadas: ${horasTrab} horas, Salario Semanal: ${salarioSemanal}`);
